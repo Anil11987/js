a19918d951cc4a55f5eb4fc482b7e90ed2d07f4e
Anil Kumar <anilkumar11987@gmail.com>
2025-05-23 13:18:24 +0000

    object type

---
+++ test.js
@@ -0,0 +1,30 @@
+let score = false
+
+// console.log(typeof (score));
+
+let valueInNumber = Number (score);
+
+// console.log(valueInNumber);
+
+let someNumber = 88
+
+let stringNumber = String(someNumber);
+// console.log(stringNumber);
+//  console.log(typeof stringNumber)
+
+
+let value = 25
+let negValue = -value
+// console.log(negValue)
+
+//console.log(2%2)
+
+// console.log(2 + 2 + "2");
+// console.log("2" + 2 + 2);
+
+let gameCounter = 100
+
+++gameCounter;
+
+console.log(gameCounter)
+
